9c454f2a570dc286b558ef88240d91cf1fd803ec
toitek <issawandhe@gmail.com>
2022-12-31 15:43:25 +0000

    Modified the scraping logic

--- main.ts
+++ main.ts
@@ -1,27 +1,35 @@
 import puppeteer from 'puppeteer';
 
-
 async function scrapeJobs(url: string) {
-    // Launch a new browser instance
-    const browser = await puppeteer.launch();
-    // Create a new page
-    const page = await browser.newPage();
-    // Navigate to the given URL
-    await page.goto(url);
-    // Extract the job information from the page
-    const jobs = await page.evaluate(() => {
-      const jobElements = document.querySelectorAll('.job-listing');
-      return Array.from(jobElements).map((jobElement: any) => {
-        const title = jobElement.querySelector('.job-title').innerText;
-        const link = jobElement.querySelector('.job-apply-button').href;
-        return { title, link };
-      });
+  // Launch a headless browser
+  const browser = await puppeteer.launch();
+
+  // Open a new page
+  const page = await browser.newPage();
+
+  // Navigate to the given URL
+  await page.goto(url);
+
+  // Extract the job information from the page
+  const jobs = await page.evaluate(() => {
+    const jobElements = document.querySelectorAll('.job-listing');
+    return Array.from(jobElements).map(element => {
+      const titleElement = element.querySelector('.job-title');
+      const title = titleElement ? titleElement.textContent : '';
+
+      const applicationLinkElement = element.querySelector('.apply-button');
+      const applicationLink = applicationLinkElement ? applicationLinkElement.getAttribute('href') : '';
+
+      return { title, applicationLink };
     });
-    // Log the job information to the console
-    console.log(jobs);
-    // Close the browser when you're done
-    await browser.close();
-  }
+  });
+
+  // Log the job information to the console
+  console.log(jobs);
+
+  // Close the browser and end the Puppeteer session
+  await browser.close();
+}
   
 
   scrapeJobs('https://www.builtinnyc.com/jobs');
