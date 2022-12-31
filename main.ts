38760e5c059c40f2676dbaae6f11788b5df51662
toitek <issawandhe@gmail.com>
2022-12-31 15:45:47 +0000

    Added a feature to handle pagination and support multiple types of job board pages

--- main.ts
+++ main.ts
@@ -10,22 +10,39 @@ async function scrapeJobs(url: string) {
   // Navigate to the given URL
   await page.goto(url);
 
-  // Extract the job information from the page
-  const jobs = await page.evaluate(() => {
-    const jobElements = document.querySelectorAll('.job-listing');
-    return Array.from(jobElements).map(element => {
-      const titleElement = element.querySelector('.job-title');
-      const title = titleElement ? titleElement.textContent : '';
-
-      const applicationLinkElement = element.querySelector('.apply-button');
-      const applicationLink = applicationLinkElement ? applicationLinkElement.getAttribute('href') : '';
-
-      return { title, applicationLink };
+  // Define a function to extract the job information from the current page
+  const extractJobs = async () => {
+    // Extract the job information from the page
+    const jobs = await page.evaluate(() => {
+      const jobElements = document.querySelectorAll('.job-listing');
+      return Array.from(jobElements).map(element => {
+        const titleElement = element.querySelector('.job-title');
+        const title = titleElement ? titleElement.textContent : '';
+  
+        const applicationLinkElement = element.querySelector('.apply-button');
+        const applicationLink = applicationLinkElement ? applicationLinkElement.getAttribute('href') : '';
+  
+        return { title, applicationLink };
+      });
     });
-  });
+  
+    // Log the job information to the console
+    console.log(jobs);
+  };
 
-  // Log the job information to the console
-  console.log(jobs);
+  // Extract the job information from the current page
+  await extractJobs();
+
+  // Check if there is a "next" button to paginate to the next page
+  const nextButtonSelector = '.pagination-next';
+  if (await page.$(nextButtonSelector)) {
+    // If the "next" button exists, click it and extract the job information from the next page
+    while (await page.$(nextButtonSelector)) {
+      await page.click(nextButtonSelector);
+      await page.waitForSelector(nextButtonSelector);
+      await extractJobs();
+    }
+  }
 
   // Close the browser and end the Puppeteer session
   await browser.close();
