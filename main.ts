ff337bdb3419eb55eef88321e3f66498a4601d44
toitek <issawandhe@gmail.com>
2022-12-31 15:56:06 +0000

    modified the funtion to be able to handle  different job boards

--- main.ts
+++ main.ts
@@ -12,19 +12,51 @@ async function scrapeJobs(url: string) {
 
   // Define a function to extract the job information from the current page
   const extractJobs = async () => {
-    // Extract the job information from the page
-    const jobs = await page.evaluate(() => {
-      const jobElements = document.querySelectorAll('.job-listing');
-      return Array.from(jobElements).map(element => {
-        const titleElement = element.querySelector('.job-title');
-        const title = titleElement ? titleElement.textContent : '';
-  
-        const applicationLinkElement = element.querySelector('.apply-button');
-        const applicationLink = applicationLinkElement ? applicationLinkElement.getAttribute('href') : '';
-  
-        return { title, applicationLink };
+    // Check the structure of the page and use the appropriate DOM selectors and functions to extract the job information
+    let jobs;
+    if (await page.$('.job-listing')) {
+      // Extract the job information from a page with the expected structure
+      jobs = await page.evaluate(() => {
+        const jobElements = document.querySelectorAll('.job-listing');
+        return Array.from(jobElements).map(element => {
+          const titleElement = element.querySelector('.job-title');
+          const title = titleElement ? titleElement.textContent : '';
+    
+          const applicationLinkElement = element.querySelector('.apply-button');
+          const applicationLink = applicationLinkElement ? applicationLinkElement.getAttribute('href') : '';
+    
+          return { title, applicationLink };
+        });
       });
-    });
+    } else if (await page.$('.job')) {
+      // Extract the job information from a page with a different structure
+      jobs = await page.evaluate(() => {
+        const jobElements = document.querySelectorAll('.job');
+        return Array.from(jobElements).map(element => {
+          const titleElement = element.querySelector('.title');
+          const title = titleElement ? titleElement.textContent : '';
+    
+          const applicationLinkElement = element.querySelector('.apply-button');
+          const applicationLink = applicationLinkElement ? applicationLinkElement.getAttribute('href') : '';
+    
+          return { title, applicationLink };
+        });
+      });
+    } else {
+      // Extract the job information from a page with a different structure
+      jobs = await page.evaluate(() => {
+        const jobElements = document.querySelectorAll('.position');
+        return Array.from(jobElements).map(element => {
+          const titleElement = element.querySelector('.title');
+          const title = titleElement ? titleElement.textContent : '';
+    
+          const applicationLinkElement = element.querySelector('.apply-button');
+          const applicationLink = applicationLinkElement ? applicationLinkElement.getAttribute('href') : '';
+    
+          return { title, applicationLink };
+        });
+      });
+    }
   
     // Log the job information to the console
     console.log(jobs);
